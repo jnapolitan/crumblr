cb321bf3116b1153067aa8c3f8182094a7b11266
Julian Napolitan <jnapolitan@squareup.com>
2018-03-28 11:54:44 -0700

    updated with images

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214053509) do
+ActiveRecord::Schema.define(version: 20180328183414) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
